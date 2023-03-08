259de27d04a56fcb566c1563d4830b2828cba48a
DimitrisApostolakis <DimitrisApostolakis@AA-BQ74SL3>
2023-03-08 15:16:22 +0200

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,31 @@
+using Microsoft.Playwright;
+
+namespace PlaywrightDemo;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        //Playwright
+        using var playwright = await Playwright.CreateAsync();
+        //Browser
+        await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false
+        });
+        //Page
+        var page = await browser.NewPageAsync();
+        await page.GotoAsync("http://www.eaapp.somee.com");
+        await page.ClickAsync("text=Login");
+        await page.ScreenshotAsync(new PageScreenshotOptions
+        {
+            Path = "EAApp.jpg"
+        });
+    }
+}
